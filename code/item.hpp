c50d91db570613a494683683e29321c9f183c63b
Yung-Yu Chen <yyc@solvcon.net>
2021-07-05 21:58:30 +0800

    Correct inclusion

--- code/item.hpp
+++ code/item.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <boost/enable_shared_from_this.hpp>
-#include <boost/smart_ptr/make_shared.hpp>
+#include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
 #include <memory>
 
